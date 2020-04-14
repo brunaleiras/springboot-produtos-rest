6f2186e8781edd8431129063da41297eac097f11
Bruna Letycia Batista Leiras <bruna.leiras@ectec02nl.culturainglesa.int>
2020-04-14 17:45:09 -0300

    aprimorando o rect app para consumir a API REST, add axios, criacao dos components Loja e ListaProdutos

---
+++ frontend/src/component/LojaApp.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ListaProdutosComponent from './ListaProdutosComponent'
+class LojaApp extends Component {
+    render() {
+        return (<>
+              <h1>E-commerce Application</h1>
+              <ListaProdutosComponent/>
+            </>
+        )
+    }
+}
+
+export default LojaApp
